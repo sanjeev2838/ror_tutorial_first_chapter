54d2389843ae6e17a86c35060d7eb6b70c66a296
sanjeevk <Sanjeevk.impinge@gmail.com>
2013-11-18 15:41:28 +0530

    renamed welcomes to welcome

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+
+  # GET /welcome
+  # GET /welcome.json
+  def index
+
+  end
+
+end
--- app/controllers/welcomes_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomesController < ApplicationController
-
-  # GET /welcomes
-  # GET /welcomes.json
-  def index
-
-  end
-
-end
